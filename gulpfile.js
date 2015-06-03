7917676d63c4d162b048e071c60487db2d67c1d8
Gavin Ching <gavinchingy@gmail.com>
2015-06-02 23:52:06 -0700

    Add in compiler and minify task for gulp and change all production files to be in dist

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 var gulp = require('gulp');
 var minifyCss = require('gulp-minify-css');
+var ejs = require("gulp-ejs");
+var minifyHTML = require('gulp-minify-html');
 
 
 /** Path definitions */
 var assets_styles = './assets/stylesheets/*.css';
+var ejs_views = ['./views/*.ejs', './views/**/*.ejs'];
 
 /**
   Default task
@@ -12,8 +15,11 @@ var assets_styles = './assets/stylesheets/*.css';
   * Minifies and cleans CSS, HTML, Javascript
 */
 gulp.task('default', function(){
-  // Just watch for css changes and
+  // Watch CSS
   gulp.watch([assets_styles], ['minify-css']);
+
+  // Watch HTML
+  gulp.watch(ejs_views, ['compile-min-ejs']);
 });
 
 
@@ -22,7 +28,7 @@ gulp.task('default', function(){
   minify
   Minifies and cleans HTML, CSS, Javascript
 */
-gulp.task('minify', ['minify-css']);
+gulp.task('minify', ['minify-css', 'compile-min-ejs']);
 
 /**
   minify-css
@@ -30,6 +36,18 @@ gulp.task('minify', ['minify-css']);
 */
 gulp.task('minify-css', function(){
   return gulp.src(assets_styles)
-   .pipe(minifyCss())
-   .pipe(gulp.dest('public/stylesheets'));
+    .pipe(minifyCss())
+    .pipe(gulp.dest('public/stylesheets'))
+    .pipe(gulp.dest('dist/stylesheets'));
+});
+
+/**
+  compile-min-ejs
+  Compiles and minifies EJS views
+*/
+gulp.task('compile-min-ejs', function(){
+  return gulp.src('./views/*.ejs')
+    .pipe(ejs())
+    .pipe(minifyHTML())
+    .pipe(gulp.dest("dist"));
 });
