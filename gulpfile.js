9a72790cc5deddf9bfb8e80c690b69accdbee041
Gavin Ching <gavinchingy@gmail.com>
2015-06-02 23:24:53 -0700

    Change path of where assets should be worked on and minifies to public

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp');
 var minifyCss = require('gulp-minify-css');
 
+
+/** Path definitions */
+var assets_styles = './assets/stylesheets/*.css';
+
 /**
   Default task
   Does everything to deploy to production
@@ -9,7 +13,7 @@ var minifyCss = require('gulp-minify-css');
 */
 gulp.task('default', function(){
   // Just watch for css changes and
-  gulp.watch(['./public/stylesheets/*.css'], ['minify-css']);
+  gulp.watch([assets_styles], ['minify-css']);
 });
 
 
@@ -25,7 +29,7 @@ gulp.task('minify', ['minify-css']);
   Minifies CSS
 */
 gulp.task('minify-css', function(){
-  return gulp.src('public/stylesheets/*.css')
+  return gulp.src(assets_styles)
    .pipe(minifyCss())
-   .pipe(gulp.dest('dist/stylesheets'));
+   .pipe(gulp.dest('public/stylesheets'));
 });
